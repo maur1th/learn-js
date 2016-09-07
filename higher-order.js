38d82630cb5c986d1523a2c66a050d4741c6f93c
Thomas Maurin <thomas@maurin.io>
2016-09-07 10:10:13 +0200

    more examples

--- higher-order.js
+++ higher-order.js
@@ -43,7 +43,8 @@ for (var i = 0; i < animals.length; i++) {
   }
 }
 
-// es5 -- function composition
+// es5 -- function composition :
+// la fonction .filter prend une fonction comme argument
 dogs = animals.filter(function (animal) {
   return animal.species == 'dog'
 })
@@ -54,20 +55,9 @@ var isDog = function (animal) {
 }
 dogs = animals.filter(isDog)
 
-// es2015 -- syntactic sugar
-dogs = animals.filter( animal => animal.species == 'dog' )
-
 
 // 3. Array.map
 // http://goo.gl/8BSD2u
-animals = [
-  { name: 'Fluffykins', species: 'rabbit' },
-  { name: 'Caro',       species: 'fish' },
-  { name: 'Hamilton',   species: 'dog' },
-  { name: 'Harold',     species: 'fish' },
-  { name: 'Ursula',     species: 'dog' },
-  { name: 'Jimmy',      species: 'fish' }
-]
 
 // obtenir une liste des noms
 // pre es5
@@ -78,25 +68,79 @@ for (var i = 0; i < animals.length; i++) {
 }
 
 // es5
+var getName = function (animal) {
+  return animal.name
+}
+names = animals.map(getName)
+
+
+// 4. Chaining
+// Array.map et .filter retournant des Arrays, ces fonctions peuvent être
+// chaînées autant de fois que nécessaire
+
+// obtenir le nom des chiens
+names = animals.filter(isDog).map(getName)
+
+
+// 5. ES6 / ES2015 syntactic sugar: arrow functions
 names = animals.map(function (animal) {
   return animal.name
 })
 
-// es2015
+// les arrow functions permettent de supprimer le keyword function :
+// function () {} devient () => {}
+names = animals.map( (animal) => {
+  return animal.name
+})
+
+// si le corps de la fonction n'est constitué que d'un return statement, on
+// peut également supprimer les accolades et le return :
+names = animals.map( (animal) => animal.name )
+
+// s'il n'y a qu'un argument, on peut également supprimer ses parenthèses :
 names = animals.map( animal => animal.name )
 
+// enfin, en programmation fonctionnelle on a tendance à remplacer les noms de
+// variable par des lettres :
+names = animals.map( a => a.name )
 
-// 4. Chaining
-animals = [
-  { name: 'Fluffykins', species: 'rabbit' },
-  { name: 'Caro',       species: 'fish' },
-  { name: 'Hamilton',   species: 'dog' },
-  { name: 'Harold',     species: 'fish' },
-  { name: 'Ursula',     species: 'dog' },
-  { name: 'Jimmy',      species: 'fish' }
-]
+// in fine, on passe donc pre es5 de :
+names = []
+for (var i = 0; i < animals.length; i++) {
+  names.push(animals[i].name)
+}
 
-// obtenir le nom des chiens
-names = animals
-          .filter( animal => animal.species == 'dog' )
-          .map( animal => animal.name )
+// à :
+names = animals.map( a => a.name )
+
+// voici ce que donne l'exemple pour Array.filter une fois transformé :
+dogs = animals.filter( a => a.species == 'dog' )
+
+
+// Array.reduce
+
+// Pour effectuer des opérations plus complexes pour lesquelles filter ou map
+// ne conviennent pas
+
+// Ici pour remplacer le chainage précédent :
+names = animals.filter( a => a.species == 'dog' ).map( a => a.name )
+
+// Par :
+names = animals.reduce(function (list, animal) {
+  if ( animal.species == 'dog' ) {
+    list.push(animal.name)
+  }
+  return list
+}, [])
+
+// Ça n'est pas forcément une bonne idée car on gagne peu/pas en performance,
+// et on perd en lisibilité
+
+// Sum
+var numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+var sum = numbers.reduce( function (sum, n) {
+  return sum + n
+}, 0)
+
+// en es6
+var sum = numbers.reduce( (sum, n) => sum + n , 0)
