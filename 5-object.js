fdac46be1efa64daf7906b4b06ac0a8f13a40e26
Thomas Maurin <thomas@maurin.io>
2016-09-11 18:31:42 +0200

    object stub

--- 5-object.js
+++ 5-object.js
@@ -1,7 +1,13 @@
 Object.create
 Object.defineProperty
 Object.assign
+
 Object.keys
+
 // ES2017
+// npm install object.entries object.values
+if ( !Object.entries ) { require('object.entries').shim() }
+if ( !Object.values ) { require('object.values').shim() }
+
 Object.entries
 Object.values
