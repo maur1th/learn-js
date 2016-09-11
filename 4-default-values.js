eaca03f33710120f3feac0a9d9ddb37faca0b347
Thomas Maurin <thomas@maurin.io>
2016-09-11 17:20:49 +0200

    Add default values

--- 4-default-values.js
+++ 4-default-values.js
@@ -1,5 +1,7 @@
+const assert = require('assert')
+
 // Il est possible d'attribuer une valeur par défaut à un argument
-// cette valeur par défaut remplacera tout argument dont la valeur est undefined
+// cette valeur remplacera tout argument dont la valeur est `undefined`
 var f = function (n=2) {
   return n
 }
@@ -8,3 +10,18 @@ assert(f() === 2)           // => 2
 assert(f(3) === 3)          // => 2
 assert(f(null) === null)    // => null
 assert(f(undefined) === 2)  // => 2
+
+
+// Fonctionne également avec les destructuring assignments
+const process = ({date, summary, location:place='Home'}) => {
+  assert(date === 'YYYY-MM-DD HH:mm:ss')
+  assert(summary === 'Lunch with Sarah')
+  assert(place === 'Home')
+}
+
+const query = {
+  date: 'YYYY-MM-DD HH:mm:ss',
+  summary: 'Lunch with Sarah',
+}
+
+process(query)
