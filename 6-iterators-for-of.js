dc2e20decebd78188ffb979007fc7bee7fefcc1c
Thomas Maurin <thomas@maurin.io>
2016-09-11 17:38:17 +0200

    Update iterators for of with assert

--- 6-iterators-for-of.js
+++ 6-iterators-for-of.js
@@ -1,5 +1,7 @@
+const assert = require('assert')
+
 // es2015
-var list = ['apples', 'oranges', 'kiwis']
+const list = ['apples', 'oranges', 'kiwis']
 list.name = 'groceries'
 
 // Array.map, filter, reduce et forEach ne conviennent pas toujours : il est
@@ -7,49 +9,82 @@ list.name = 'groceries'
 // `return` ou d'un `break`
 
 // Seule bonne façon de faire avant es6
-for ( var i = 0; i < list.length; i++ ) {
-  console.log(list[i])
-}
-console.log()
+{
+  const res = []
+  for ( var i = 0; i < list.length; i++ ) {
+    res.push(list[i])
+  }
 
-// for-in est mauvais parce que :
+  assert.deepEqual(res, ['apples', 'oranges', 'kiwis'])
+}
+
+// L'autre façon de faire, for-in n'est pas une bonne pratique parce que :
 // - i est une string et non un Number
 // - for-in itère également sur les propriétés
 // - for-in n'itère pas dans l'ordre dans certaines circonstances
-for ( var i in list ) {
-  console.log(list[i])
+{
+  const res = []
+  for ( var i in list ) {
+    res.push(list[i])
+  }
+
+  assert(res.includes('groceries'))
 }
-console.log()
 
 // for-of
 // - plus concis que for ( var i = 0; i < list.length; i++ ) {}
 // - sans les problèmes de for-in
 // - fonctionne avec break, continue et return
-for ( var fruit of list ) {
-  console.log(fruit)
+{
+  const res = []
+  for ( var fruit of list ) {
+    res.push(fruit)
+  }
+
+  assert.deepEqual(res, ['apples', 'oranges', 'kiwis'])
 }
-console.log()
 
 // for-of fonctionne également sur d'autres collections
-for ( var chr of "abc" ) {
-  console.log(chr)
+{
+  const res = []
+  for ( var chr of "abc" ) {
+    res.push(chr)
+  }
+
+  assert.deepEqual(res, ['a', 'b', 'c'])
 }
-console.log()
+
 
 // Set est une collection dont chaque élément est unique
-assert(new Set([1, 2, 3, 3]) === new Set([1, 2, 3]))
-for ( var n of new Set([1, 1, 2, 3]) ) {
-  console.log(n)
+{
+  const res = []
+  for ( var n of new Set([1, 1, 2, 3]) ) {  // Set { 1, 2, 3 }
+    res.push(n)
+  }
+
+  assert.deepEqual(res, [1, 2, 3])
 }
-console.log()
+
 
 // Map est une collection de paires clés valeurs
-for ( var [fruit, n] of new Map([['oranges', 2], ['apples', 3]]) ) {
-  console.log(fruit, n)
-})
+{
+  const res = []
+  for ( var [fruit, n] of new Map([['oranges', 2], ['apples', 3]]) ) {
+    res.push('There are ' + n + ' ' + fruit)
+  }
+
+  assert.deepEqual(res, ['There are 2 oranges', 'There are 3 apples'])
+}
 
 // Si vous n'utilisez pas de Map, il est possible de faire quelque chose de
 // similaire avec Object.entries (es2017)
-for ( var [fruit, n] of Object.entries({ oranges: 2, apples: 3 }) ) {
-  console.log(fruit, n)
+if ( !Object.entries ) { require('object.entries').shim() }
+
+{
+  const res = []
+  for ( var [fruit, n] of Object.entries({ oranges: 2, apples: 3 }) ) {
+    res.push('There are ' + n + ' ' + fruit)
+  }
+
+  assert.deepEqual(res, ['There are 2 oranges', 'There are 3 apples'])
 }
